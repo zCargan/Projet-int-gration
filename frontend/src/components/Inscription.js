75d933f01a3578532696a2854a5b119585a55bdc
Logan <lo.carlier@students.ephec.be>
2022-11-13 16:11:05 +0100

    US04 : Hashage, protection XSS et validation des entrées utilisateur
    Hashage des mots de passe en frontend enfin qu'ils ne passent pas en
    clair, protection XSS afin d'empêcher l'utilisation de scripts et
    validation des données utilisateur afin que tous les champs soient
    vérifiés avant la validation de la création de compte

--- frontend/src/components/Inscription.js
+++ frontend/src/components/Inscription.js
@@ -6,60 +6,6 @@ import emailjs from 'emailjs-com';
 import { useNavigate } from 'react-router-dom';
 import bcrypt from 'bcryptjs'
 
-
-export function notXSSInjection(string) {
-    return !(string.includes("<"))
-}
-
-export function notToLongString(string) {
-    return (string.length < 30)
-}
-
-export function checkEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
-export function allComplete(string1, string2, string3, string4) {
-    return !((string1 === "") || (string2 === "") || (string3 === "") || (string4 === ""))
-}
-
-export function allNotToLong(string1, string2, string3, string4) {
-    return (notToLongString(string1) && notToLongString(string2) && notToLongString(string3) && notToLongString(string4))
-}
-
-export function allNotXSSInjection(string1, string2, string3, string4) {
-    return (notXSSInjection(string1) && notXSSInjection(string2) && notXSSInjection(string3) && notXSSInjection(string4))
-}
-
-export function sameString(string1, string2) {
-    return (string1 === string2)
-}
-
-export function HasValidLength(string) {
-    return (string.length >= 12)
-}
-
-export function HasLowerCaseLetter(string) {
-    return (/[a-z]/.test(string))
-}
-
-export function HasUpperCaseLetter(string) {
-    return (/[A-Z]/.test(string))
-}
-
-export function HasSpecialCharacter(string) {
-    return (/[!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?]/.test(string))
-}
-
-export function HasNumber(string) {
-    return (/[0-9]/.test(string))
-}
-
-export function HasAll(string) {
-    return (HasValidLength(string) && HasLowerCaseLetter(string) && HasUpperCaseLetter(string) && HasSpecialCharacter(string) && HasNumber(string))
-}
-
 const Inscription = () => {
 
     const [username, setUsername] = useState("");
@@ -102,6 +48,10 @@ const Inscription = () => {
             password,
             samePassword
         }
+
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
         const données_envoyées = {
             "username": username,
             "email": email,
@@ -113,25 +63,43 @@ const Inscription = () => {
 
         //variable nécessaire afin d'effectuer à la requete à la db afin de savoir si l'email est déja utilisé ou non
 
-        if (allComplete(username, email, password, samePassword)) {
-            if (allNotToLong(username, email, password, samePassword)) {
-                if (allNotXSSInjection(username, email, password, samePassword)) {
-                    if (HasAll(password)) {
-                        axios.post("http://localhost:3001/user/username", infos)
+        function notXSSInjection(string) {
+            return !(string.includes("<"))
+        }
+
+        function notToLongString(string) {
+            return (string.length < 30)
+        }
+
+        function checkEmail(email) {
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        }
+
+
+        if ((username === "") || (email === "") || (password === "") || (samePassword === "")) {
+            alert("Veuillez compléter tous les champs");
+        } else {
+            if (notToLongString(username) && notToLongString(email) && notToLongString(password) && notToLongString(samePassword)) {
+                if (notXSSInjection(username) && notXSSInjection(email) && notXSSInjection(password) && notXSSInjection(samePassword)) {
+                    if (passwordHasValidLength && passwordHasLowercaseLetter && passwordHasUppercaseLetter && passwordHasSpecialCharacter && passwordHasNumber) {
+                        axios.post("http://localhost:3001/username", infos)
                             .then(response => {
-                                if (response.data === false) {
+                                if (response.data === "not ok") {
                                     alert("Username already used");
                                 } else {
                                     if (checkEmail(email)) {
-                                        axios.post("http://localhost:3001/user/email", infos)
+                                        axios.post("http://localhost:3001/email", infos)
                                             .then(response => {
-                                                if (response.data === false) {
+                                                if (response.data === "not ok") {
                                                     alert("Email already used")
                                                 } else {
-                                                    if (sameString(password, samePassword)) {
-                                                        if (HasValidLength(password)) {
+                                                    if (password === samePassword) {
+                                                        if (Number(password.length) < 12) {
+                                                            alert("Mot de passe trop court")
+                                                        } else {
                                                             axios
-                                                                .post("http://localhost:3001/user/inscription", données_envoyées)
+                                                                .post("http://localhost:3001/inscription", données_envoyées)
                                                                 .then(response => {
                                                                     if (response.status === 201) {
                                                                         emailjs.sendForm('service_wco0ss6', 'template_f9ar9zo', e.target, 'uX_z-9_6PbAb24o0e')
@@ -141,12 +109,10 @@ const Inscription = () => {
                                                                                 console.log(error.text);
                                                                             });
                                                                         e.target.reset()
-                                                                        alert("Compte créé ! Un email de confirmation vous a été envoyé")
+                                                                        alert("Compte créer ! Un email de confirmation vous a été envoyé")
                                                                         navigateToHome()
                                                                     }
                                                                 });
-                                                        } else {
-                                                            alert("Mot de passe trop court")
                                                         }
                                                     } else {
                                                         alert("Vos deux mots de passe ne correspondent pas")
@@ -167,9 +133,6 @@ const Inscription = () => {
             } else {
                 alert("Les informations que vous rentrer sont trop longue")
             }
-
-        } else {
-            alert("Veuillez compléter tous les champs");
         }
     }
     return (
