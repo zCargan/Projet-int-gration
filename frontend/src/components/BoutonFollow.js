9e8e1a5d86e2b8e1a123e64aaaf151a8b0991dd5
Logan <lo.carlier@students.ephec.be>
2022-12-21 11:08:31 +0100

    FIX(Follow): Bouton Follow non fonctionnel
    Le bouton follow ne fonctionnait pas. On ne pouvait pas follow/unfollow les
    personnes car l'id de l'utilisateur était manquant dans le lien vers le
    backend.
    
    étapes pour reproduire le bug :
    
    1) Se connecter à l'application
    2) Aller dans fil d'actualité
    3) Rechercher une personne
    4) Cliquer sur son profil
    5) Appuyer sur le bouton "Suivre"
    
    => Nous n'arrivons pas à suivre une personne
    => Il devrait follow/unfollow cette personne en fonction du status
    précédent

--- frontend/src/components/BoutonFollow.js
+++ frontend/src/components/BoutonFollow.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState , useLayoutEffect} from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import axios from 'axios';
 import { useLocation } from "react-router-dom";
 import '../styles/boutonfollow.css';
 
 
 
-function ButtonFollow (params){
+function ButtonFollow(params) {
 
 
-    
+
     const userId = useLocation();
     const [bouton, setBouton] = useState(false)
     const [newUsersFollows, setData] = useState([]);
@@ -20,38 +20,39 @@ function ButtonFollow (params){
 
     useEffect(() => {
         axios.get('http://localhost:3001/getcookie', { withCredentials: true }).then(res => {
-            idSession=res.data.Id
-              axios.get(`http://localhost:3001/session/${idSession}`,{ params: { "id": idSession }}).then(response => {
+            idSession = res.data.Id
+            axios.get(`http://localhost:3001/session/${idSession}`, { params: { "id": idSession } }).then(response => {
+                setId(response.data.idUser)
                 axios.get(`http://localhost:3001/user/${response.data.idUser}`).then(res => {
-                   setData(res.data.userfollows)
-                    if(res.data.userfollows.includes(userId.state._id)){
+                    setData(res.data.userfollows)
+                    if (res.data.userfollows.includes(userId.state._id)) {
                         setBouton(false);
                     }
-                    else{
+                    else {
                         setBouton(true);
                     };
 
+                })
             })
-          })
 
-          
+
         }).catch(err => console.log(err));
     }, [bouton])
 
 
-    
-    
-    
 
-    function suivreFollow(){
-        if( newUsersFollows.includes(userId.state._id) ){
-            newUsersFollows.splice(newUsersFollows.indexOf(userId.state._id),1)
-            axios.put(`http://localhost:3001/user/${id}`, {userfollows : newUsersFollows}).then(alert("Utilisateur retiré"))
+
+
+
+    function suivreFollow() {
+        if (newUsersFollows.includes(userId.state._id)) {
+            newUsersFollows.splice(newUsersFollows.indexOf(userId.state._id), 1)
+            axios.put(`http://localhost:3001/user/follow/${id}`, { userfollows: newUsersFollows }).then(alert("Utilisateur retirÃ©"))
             setBouton(true)
         }
         else {
             newUsersFollows.push(userId.state._id);
-            axios.put(`http://localhost:3001/user/follow/${id}`, { userfollows : newUsersFollows }).then(alert("Utilisateur suivi!"))
+            axios.put(`http://localhost:3001/user/follow/${id}`, { userfollows: newUsersFollows }).then(alert("Utilisateur suivi!")).catch(error => { console.log(error) })
             setBouton(false)
         }
 
@@ -60,16 +61,15 @@ function ButtonFollow (params){
 
 
 
-    return(
+    return (
         <div>
             <button className='boutonFollow' onClick={suivreFollow}>{bouton ? 'Suivre' : 'Ne plus suivre'}</button>
         </div>
-        )
-    
+    )
 
-        
+
+
 
 }
 
 export default ButtonFollow;
-
